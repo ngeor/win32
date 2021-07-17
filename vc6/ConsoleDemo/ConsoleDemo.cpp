fe6ce77ab46d2b43a5d7177d10b051f435490f5e
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-07-17 07:42:54 +0200

    Added StaticLibraryDemo

--- vc6/ConsoleDemo/ConsoleDemo.cpp
+++ vc6/ConsoleDemo/ConsoleDemo.cpp
@@ -3,11 +3,14 @@
 
 #include "stdafx.h"
 #include "..\DynamicLibraryDemo\DynamicLibraryDemo.h"
+#include "..\StaticLibraryDemo\StaticLibraryDemo.h"
 
 int main(int argc, char *argv[])
 {
 	printf("Hello World!\n");
 	int x = Add(1, 3);
 	printf("Add result is %d\n", x);
+	int y = Subtract(9, 1);
+	printf("Subtract result is %d\n", y);
 	return 0;
 }
---
+++ vc6/StaticLibraryDemo/StaticLibraryDemo.h
@@ -0,0 +1,21 @@
+// StaticLibraryDemo.h: interface for the StaticLibraryDemo class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_STATICLIBRARYDEMO_H__950B625D_1F56_482F_BCC5_9693F11F1786__INCLUDED_)
+#define AFX_STATICLIBRARYDEMO_H__950B625D_1F56_482F_BCC5_9693F11F1786__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class StaticLibraryDemo
+{
+public:
+	StaticLibraryDemo();
+	virtual ~StaticLibraryDemo();
+};
+
+int Subtract(int a, int b);
+
+#endif // !defined(AFX_STATICLIBRARYDEMO_H__950B625D_1F56_482F_BCC5_9693F11F1786__INCLUDED_)
