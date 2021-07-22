78bfd9a85d184a720797b57e2e31162b39f312fc
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-07-22 09:05:57 +0200

    LoadString seems to be working differently for Windows 2000, not possible to get a readonly reference to the string resource directly.

--- vc6/WinObj/Instance.cpp
+++ vc6/WinObj/Instance.cpp
@@ -23,14 +23,17 @@ CInstance::~CInstance()
 LPTSTR CInstance::LoadString(int id) const
 {
 	HINSTANCE hInstance = GetHandle();
+#if _MSC_VER > 1200
 	LPTSTR readOnlyBuffer;
 	int len = ::LoadString(hInstance, id, (LPTSTR)&readOnlyBuffer, 0);
 	// because readOnlyBuffer isn't guaranteed to be null terminated
 	LPTSTR result = (LPTSTR)calloc(sizeof(TCHAR), len + 1);
-#if _MSC_VER > 1200
 	_tcsncpy_s(result, len + 1, readOnlyBuffer, len);
 #else
-	_tcsncpy(result, readOnlyBuffer, len);
+	TCHAR buffer[256];
+	int len = ::LoadString(hInstance, id, buffer, 256);
+	LPTSTR result = (LPTSTR)calloc(sizeof(TCHAR), len + 1);
+	_tcsncpy(result, buffer, len);
 #endif
 	return result;
 }
